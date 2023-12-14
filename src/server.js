338c5979dea2bd3064ebfbaf5ab87c7862c07730
Eduardo Flavio de Carvalho <edufcarvalho@gmail.com>
2023-12-13 23:06:39 -0300

    routes

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
 import http from "node:http";
 import { json } from "./middlewares/json.js";
-import {routes} from './routes.js';
+import {routes} from './routes.js';   
+
+// Query Parameters: URL Stateful => Filtros, paginação, etc (não obrigatorios)
+// Route Parameters: Identificação de recursos
+// Request Body: envio de infos via formulario (passa pelo https)
+
+// http://localhost:3333/users?userId=1
+// DELETE http://localhost:3333/users/1
+// POST http://localhost:3333/users
 
 const server = http.createServer(async (req, res) => {
   const { method, url } = req
